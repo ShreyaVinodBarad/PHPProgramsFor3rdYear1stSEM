c72c147bbfce8969159a421e6f4489aaeb942992
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2022-11-23 13:46:51 +0530

    FormHandlingWithPostMethod

---
+++ PostFormHandling.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Form Handing with Post Method</title>
+    </head>
+    <body>
+        <form action="Post_Form_Handling.php" method="post">
+            <label for="name">Name</label>
+            <input type="text" name="name"/>
+            <label for="email">Email</label>
+            <input type="email" name="email"/>
+            <label for="password">Password</label>
+            <input type="password" name="password"/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>
