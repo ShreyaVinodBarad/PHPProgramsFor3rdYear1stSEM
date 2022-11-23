48614f928fbf1314aecccd4eadd9834bf28ae237
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2022-11-23 11:38:11 +0530

    FileHandlingWithGetMethod

---
+++ GetFormHandling.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Get Method for Form Handling</title>
+    </head>
+    <body>
+        <form action="Get_Form_Handling.php" method="get">
+            <label for="name">Name</label>
+            <input type="text" name="name"/>
+            <label for="email">Email</label>
+            <input type="email" name="email"/>
+            <label for="password">Password</label>
+            <input type="password" name="password"/>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>
---
+++ Get_Form_Handling.php
@@ -0,0 +1,11 @@
+<?php
+$a=$_GET["name"];
+$b=$_GET["email"];
+$c=$_GET["password"];
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+?>
