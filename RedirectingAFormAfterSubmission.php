9c268c91baf18b2d97708a40d03b12203332979b
ShreyaVinodBarad <shreyavbarad72@gmail.com>
2022-11-24 08:55:39 +0530

    RedirectingAFormAfterSubmissionCompleted

---
+++ RedirectingAFormAfterSubmission.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Redirecting A Form After Submission</title>
+</head>
+<body>
+    <h1><b>Redirecting A Form After Submission</b></h1>
+    <form action="https://google.com" method="get">
+        <label for="name">Name</label>
+        <input type="text" name="name">
+        <label for="email">Email</label>
+        <input type="email" name="email">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <input type="Submit">
+    </form>
+</body>
+</html>
